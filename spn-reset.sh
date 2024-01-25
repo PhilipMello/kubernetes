8a9c670cb593c3c2f138daee308365204d2bc7da
Philip Mello <110171839+PhilipMello@users.noreply.github.com>
2024-01-25 23:53:00 +0000

    💫Update (spn-reset.sh)

--- spn-reset.sh
+++ spn-reset.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-read -p "Type your Resource Group: " RG
-read -p "Type your cluster name: " AKS
-
-SP_ID=$(az aks show --resource-group $RG --name $AKS --query servicePrincipalProfile.clientId -o tsv)
-
-SP_SECRET=$(az ad app credential reset --id "$SP_ID" --query password -o tsv)
-
-SP_UPDATE=$(az aks update-credentials --resource-group $RG --name $AKS --reset-service-principal --service-principal "$SP_ID" --client-secret "${SP_SECRET}")
-
 GREEN="\033[97;102m"
 RED="\033[97;41m"
 ENDCOLOR="\e[0m"
+
+read -p "Type your Resource Group: " RG
+read -p "Type your Cluster name: " AKS
 
 echo -e "
 +---------------------------------------------+
 |${RED}Status: Updating Service Principal Name (SPN)${ENDCOLOR}|
 +---------------------------------------------+
 "
+
+SP_ID=$(az aks show --resource-group $RG --name $AKS --query servicePrincipalProfile.clientId -o tsv)
+
+SP_SECRET=$(az ad app credential reset --id "$SP_ID" --query password -o tsv)
+
+SP_UPDATE=$(az aks update-credentials --resource-group $RG --name $AKS --reset-service-principal --service-principal "$SP_ID" --client-secret "${SP_SECRET}")
 
 eval $SP_UPDATE
 
