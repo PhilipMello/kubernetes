eec1818261d4ba2aac88956e6a45937588cdaa5b
Philip Mello <110171839+PhilipMello@users.noreply.github.com>
2024-01-26 21:39:54 +0000

    💫Update (spn-reset.sh)

--- spn-reset.sh
+++ spn-reset.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
+# Author
+author="Philip Mello <@Microsoft>"
+# Version
+version="1.0"
+# License
+license="MIT"
+# Date
+current_date=$(date +'%b %d, %Y')
+# GitHub
+github='https://github.com/PhilipMello/k8s/'
+# Microsoft Official Documentation
+microsoft='https://learn.microsoft.com/en-us/azure/aks/update-credentials#update-or-create-a-new-service-principal-for-your-aks-cluster'
+echo "
+# --------------------------------------------------------------
+# Script     :
+# Description:
+# Version    : $version
+# Date       : $current_date
+# Author     : $author
+# License    : $license
+# Github     : $github
+# Microsoft  : $microsoft
+# --------------------------------------------------------------
+# How to use:
+# Exemples:
+# --------------------------------------------------------------
+"
 
 GREEN="\033[97;102m"
 RED="\033[97;41m"
