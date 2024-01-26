89c55391975460e52b9298a733af40111fd5ee5d
Philip Mello <110171839+PhilipMello@users.noreply.github.com>
2024-01-26 21:09:49 +0000

    ➕Create (header-template.sh)

---
+++ header-template.sh
@@ -0,0 +1,21 @@
+# Author
+author="Philip Mello <@Microsoft>"
+# Version
+version="1.0"
+# License
+license="MIT"
+# Date
+current_date=$(date +'%b %d, %Y')
+echo "
+# --------------------------------------------------------------
+# Script     :
+# Description:
+# Version    : $version
+# Date       : $current_date
+# Author     : $author
+# License    : $license
+# --------------------------------------------------------------
+# How to use:
+# Exemples:
+# --------------------------------------------------------------
+"
